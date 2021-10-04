4fd24ff64e9aba2a284bf8d2b2df73de6c6a4eac
grostaco <grostaco@gmail.com>
2021-10-04 20:23:24 +0700

    refactor(rpg): Removed Player from type hints

--- rpg/script.py
+++ rpg/script.py
@@ -1,16 +1,19 @@
 from __future__ import annotations
 
-from typing import List, Tuple, Union, Optional
+from typing import List, Tuple, Union, Optional, Iterable, Literal
+from itertools import cycle, chain
 
-from .characters import Character, Player
+from .characters import Character
+from operator import attrgetter
+from .util import as_gen
 
 
 class Script:
     def __init__(self):
-        self.protagonists: List[Player] = []
+        self.protagonists: List[Character] = []
         self.story: List[Union[Dialogue, Choice]] = []
 
-    def add_protagonist(self, protagonist: Player):
+    def add_protagonist(self, protagonist: Character):
         self.protagonists.append(protagonist)
 
     def add_dialogue(self, dialogue: Dialogue):
@@ -67,3 +70,44 @@ class Choice:
 
     def select(self, choice: str):
         return next((dialogue for dialogue in self.dialogues if dialogue[0] == choice), None)
+
+
+class Fight:
+    def __init__(self, left: Iterable[Character], right: Iterable[Character]):
+        self.left = left
+        self.right = right
+        self.lookup = tuple(chain.from_iterable([left, right]))
+        self.name_lookup = {character.name: character for character in self.lookup}
+        self.turns = cycle(self.lookup)
+        self.current: Optional[Character] = None
+
+    def next_turn(self) -> Optional[Character]:
+        if any(map(attrgetter('stats.hp'), self.left)) or any(map(attrgetter('stats.hp'), self.right)):
+            return None
+
+        self.update_effects()
+        self.current = next(self.turns)
+        return self.current
+
+    # should only be called if next_turn returns None
+    def winner(self) -> Union[Literal['left'], Literal['right']]:
+        if any(map(attrgetter('stats.hp'), self.left)):
+            return 'left'
+        return 'right'
+
+    def turn_action(self, skill_name: str, target_names: Optional[str, List[str]]):
+        skill = next((skill for skill in self.current.skills if skill.name == skill_name), None)
+        targets = (self.name_lookup[target] for target in as_gen([target_names]))
+        if not skill:
+            raise ValueError(f'Skill name {skill_name} does not exist for character {self.current.name}')
+        if isinstance(target_names, str):
+            targets = next(targets, None)
+        elif target_names is None:
+            targets = None
+
+        skill.use(self.current, targets)
+
+    def update_effects(self):
+        for character in self.lookup:
+            for effect in character.effects:
+                effect.modify(character)
