01ac0c4046d137f3bda1d232addc7c41dc5bc60d
grostaco <grostaco@gmail.com>
2021-10-04 20:24:00 +0700

    feat(rpg): Added effects for duration and trigger based

--- rpg/effects.py
+++ rpg/effects.py
@@ -1,17 +1,36 @@
 from abc import ABCMeta, abstractmethod
+from typing import Callable, Optional
+
+
+from .characters import Character
+"""
+On Effects
+
+Is it [periodic] or [persistent]? If not, how does it [trigger]?
+What does it [do]?
+
+If duration is -1, it is persistent, else it is periodic
+"""
 
 
 class Effect(metaclass=ABCMeta):
-    def __init__(self, effect_name: str, effect_desc: str):
+    def __init__(self, effect_name: str, effect_desc: str,
+                 duration: int, trigger: Optional[Callable]):
         self.name = effect_name
         self.desc = effect_desc
+        self.duration = duration
+        self.trigger = trigger
 
     @abstractmethod
-    def modify(self, *args): ...
+    def modify(self, character: Character): ...
 
 
-class Frozen(Effect):
-    def __init__(self, effect_name: str, effect_desc: str):
-        super().__init__(effect_name, effect_desc)
+# Take 5% off of the character's health pool
+class Poison(Effect):
+    def __init__(self, effect_name: str, effect_desc: str, duration: int):
+        super().__init__(effect_name, effect_desc, duration, None)
 
-    def modify(self, *args): ...
+    def modify(self, character: Character):
+        if self.duration:
+            self.duration -= 1
+
