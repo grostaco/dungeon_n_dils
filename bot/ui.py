b2baab6003450ceb0ee8825ee4e151645130c554
grostaco <grostaco@gmail.com>
2021-10-05 10:26:29 +0700

    feat(bot): Added skill and target selection

--- bot/ui.py
+++ bot/ui.py
@@ -1,92 +1,18 @@
-from typing import List, Optional, Iterator, Any, Callable, Tuple
-from abc import ABCMeta, abstractmethod
-import rpg
-import discord
-from .util import remove_callback
 import itertools
+from typing import List, Iterator, Callable, Tuple
 
+import discord
 from discord.abc import Messageable
 from discord_components import (
     DiscordComponents,
     Button,
     ButtonStyle,
     Interaction,
-    Component,
 )
 
-
-# noinspection PyArgumentList
-class Selectable(metaclass=ABCMeta):
-    def __init__(self,
-                 client: DiscordComponents,
-                 channel: Messageable,
-                 options: List[Any],
-                 select_title: str,
-                 up_button: Optional[Button] = Button(style=ButtonStyle.blue, emoji='🔼'),
-                 down_button: Optional[Button] = Button(style=ButtonStyle.blue, emoji='🔽'),
-                 select_button: Optional[Button] = Button(style=ButtonStyle.blue, label='Select'),
-                 extra_components: List[Component] = None):
-        self.client = client
-        self.channel = channel
-        self.options = options
-        self.select_title = select_title
-        self.up_button = up_button
-        self.down_button = down_button
-        self.select_button = select_button
-        self.extra_components = extra_components or []
-        self.index = 0
-
-    def get_components(self):
-        return [
-            [
-                self.client.add_callback(
-                    self.up_button,
-                    self.button_up_callback,
-                ),
-                self.client.add_callback(
-                    self.down_button,
-                    self.button_down_callback,
-                ),
-                self.client.add_callback(
-                    self.select_button,
-                    self.select_callback,
-                ),
-            ] + self.extra_components
-        ]
-
-    def get_embed(self):
-        desc = ''
-        for i, option in enumerate(self.options):
-            if i == self.index:
-                desc += '▶️   '
-            desc += f'{option}\n'
-
-        return discord.Embed(title=self.select_title,
-                             description=desc)
-
-    async def start(self, inter: Optional[Interaction] = None):
-        if inter:
-            await inter.edit_origin(embed=self.get_embed(),
-                                    components=self.get_components())
-        else:
-            await self.channel.send(embed=self.get_embed(),
-                                    components=self.get_components())
-
-    async def button_up_callback(self, inter: Interaction):
-        self.index = (self.index + 1) % len(self.options)
-        await self.button_callback(inter)
-
-    async def button_down_callback(self, inter: Interaction):
-        self.index = (self.index - 1) % len(self.options)
-        await self.button_callback(inter)
-
-    @abstractmethod
-    async def select_callback(self, inter: Interaction):
-        ...
-
-    async def button_callback(self, inter: Interaction):
-        await inter.edit_origin(embed=self.get_embed(),
-                                components=self.get_components())
+import rpg
+from .ui_template import Selectable, TargetSelect, SkillSelect
+from .util import remove_callback, start_wait
 
 
 class Shop(Selectable):
@@ -225,11 +151,16 @@ class Fight:
         await self.inventory.start()
 
     async def begin_fight(self, inter: Interaction):
-        class _Fight:
-            def __init__(self, fight: rpg.Fight):
-                self.fight = fight
+        await inter.respond(type=7)
+        fight = rpg.Fight(self.party_one, self.party_two)
+        current = fight.next_turn()
 
-            ...
+        s = SkillSelect(self.client, self.channel, current.skills)
+
+        await start_wait(self.client.bot, s, check=lambda _inter: _inter.custom_id == 'skill_selected')
+
+        t = TargetSelect(self.client, self.channel, fight)
+        await start_wait(self.client.bot, t, check=lambda _inter: _inter.custom_id == 'target_selected')
 
     async def update(self):
         await self.original_message.edit(embed=self.get_embed())
