4c40be6ce015bffa0a871c36725d67c4ae04b99e
grostaco <grostaco@gmail.com>
2021-10-22 19:12:50 +0700

    feat: add `stat_mod` callback

--- rpg/util.py
+++ rpg/util.py
@@ -1,6 +1,10 @@
-from typing import TypeVar, Iterable, Iterator, overload, Union
+from typing import TypeVar, Iterable, Iterator, overload, Union, Callable, TYPE_CHECKING
+from .common import Stats
+from .characters import Character
 
 T = TypeVar('T')
+mod_func = Callable[[float], float]
+identity = lambda x: x
 
 
 @overload
@@ -18,4 +22,18 @@ def as_gen(x: Union[T, Iterable[T]]) -> Iterator[T]:
         yield x
 
 
+def stat_mod(
+        hp: mod_func = identity,
+        defense: mod_func = identity,
+        atk: mod_func = identity,
+        intelligence: mod_func = identity,
+) -> Callable[[Character], Stats]:
+    def _wrap(character: Character):
+        return Stats(
+            hp(character.stats.hp),
+            defense(character.stats.defense),
+            atk(character.stats.atk),
+            intelligence(character.stats.int),
+        )
 
+    return _wrap
